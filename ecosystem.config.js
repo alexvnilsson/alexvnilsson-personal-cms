c08822afdd64adedc920b31022f21bb30a0a546f
alexvnilsson <alexvnilsson@outlook.com>
2020-10-20 13:37:23 +0200

    Update ecosystem.config.js

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -3,7 +3,7 @@ const dotenv = require("dotenv").config();
 module.exports = {
   apps: [
     {
-      name: "alexvnilsson-personal-cms",
+      name: "strapi",
       script: "npm start",
       watch: true,
       env: {
