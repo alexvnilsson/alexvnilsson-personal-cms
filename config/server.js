6492621a75a4296ae0acb7a92ce6a81163b43bc8
alexvnilsson <alexvnilsson@outlook.com>
2020-10-20 14:03:42 +0200

    Update server config

--- config/server.js
+++ config/server.js
@@ -1,9 +1,17 @@
-module.exports = ({ env }) => ({
-  host: env("HOST", "0.0.0.0"),
-  port: env.int("PORT", 1337),
-  admin: {
-    auth: {
-      secret: env("ADMIN_JWT_SECRET", "9ff58f56a2c1ec02f2706c042c120f4c"),
+module.exports = ({ env }) => {
+  const _env = {
+    host: env("HOST", "0.0.0.0"),
+    port: env.int("PORT", 1337),
+  };
+
+  return {
+    host: _env.host,
+    port: _env.port,
+    url: env("URL", "http://" + _env.host + ":" + _env.port),
+    admin: {
+      auth: {
+        secret: env("ADMIN_JWT_SECRET", "9ff58f56a2c1ec02f2706c042c120f4c"),
+      },
     },
-  },
-});
+  };
+};
--- ecosystem.config.js
+++ ecosystem.config.js
@@ -1,4 +1,11 @@
-const dotenv = require("dotenv").config();
+// const dotenv = require("dotenv").config();
+
+// const envDefaults = {
+//   NODE_ENV: "production",
+//   HOST: "localhost",
+//   PORT: 1338,
+//   URL: "http://localhost:1338",
+// };
 
 module.exports = {
   apps: [
@@ -6,10 +13,6 @@ module.exports = {
       name: "strapi",
       script: "npm start",
       watch: true,
-      env: {
-        NODE_ENV: "production",
-        ...dotenv.parsed,
-      },
     },
   ],
 };
