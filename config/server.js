61ec8500e501a7fda9f5f473e3f28c8a899b2783
alexvnilsson <alexvnilsson@outlook.com>
2020-11-15 21:00:05 +0100

    Update server.js

--- config/server.js
+++ config/server.js
@@ -4,11 +4,19 @@ module.exports = ({ env }) => {
     port: env.int("PORT", 1337),
   };
 
+  const defaults = {
+    url: "http://" + _env.host + ":" + _env.port,
+    admin_url: undefined,
+  };
+
+  defaults.admin_url = defaults.url + "/dashboard";
+
   return {
     host: _env.host,
     port: _env.port,
-    // url: env("URL", "http://" + _env.host + ":" + _env.port),
+    url: env("URL", defaults.url),
     admin: {
+      url: env("ADMIN_URL", defaults.admin_url),
       auth: {
         secret: env("ADMIN_JWT_SECRET", "9ff58f56a2c1ec02f2706c042c120f4c"),
       },
